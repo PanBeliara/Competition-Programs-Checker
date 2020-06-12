5710fe64b753bd6231dc87d60176b35c8b15e34f
Tomasz Nowopolski <22752612+FeelThePoveR@users.noreply.github.com>
2020-06-12 19:03:05 +0200

    Fix Dropdownlists on main page

--- Competition Programs Checker/Default.aspx.cs
+++ Competition Programs Checker/Default.aspx.cs
@@ -12,61 +12,6 @@ namespace Competition_Programs_Checker
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            PopulateTasks();
-            PopulateLanguages();
-        }
-
-        private void PopulateTasks()
-        {
-            List<String> allProblems = null;
-
-            using (DatabaseEntities dc = new DatabaseEntities())
-            {
-                var problems = (from a in dc.Problems
-                                select new
-                                {
-                                    a
-                                });
-                if (problems != null)
-                {
-                    allProblems = new List<String>();
-                    foreach (var x in problems)
-                    {
-                        Problem u = x.a;
-                        allProblems.Add(u.title);
-                    }
-                }
-            }
-
-            DropDownList1.DataSource = allProblems;
-            DropDownList1.DataBind();
-        }
-
-        private void PopulateLanguages()
-        {
-            List<String> allLanguages = null;
-
-            using (DatabaseEntities dc = new DatabaseEntities())
-            {
-                var languages = (from a in dc.ProgrammingLanguages
-                                select new
-                                {
-                                    a
-                                });
-                if (languages != null)
-                {
-                    allLanguages = new List<String>();
-                    foreach (var x in languages)
-                    {
-                        ProgrammingLanguage u = x.a;
-                        allLanguages.Add(u.language_name);
-                    }
-                }
-            }
-
-            DropDownList2.DataSource = allLanguages;
-            DropDownList2.SelectedValue = "Python";
-            DropDownList2.DataBind();
         }
 
         protected void sendTask_Click(object sender, EventArgs e)
--- Competition Programs Checker/Default.aspx.designer.cs
+++ Competition Programs Checker/Default.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Competition_Programs_Checker
     {
 
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
@@ -22,6 +31,15 @@ namespace Competition_Programs_Checker
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
 
         /// <summary>
         /// DropDownList2 control.
